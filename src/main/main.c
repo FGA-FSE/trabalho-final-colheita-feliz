c427b03ffebf240af4db44deaecfc3bb86165a03
art-42 <artursv2001@gmail.com>
2024-09-02 10:39:29 -0300

    corrigido tempo de leitura

--- src/main/main.c
+++ src/main/main.c
@@ -58,7 +58,7 @@ void handleServerCommunication(void *params)
   {
     while (true)
     {
-      vTaskDelay(10000 / portTICK_PERIOD_MS);
+      vTaskDelay(1000 / portTICK_PERIOD_MS);
 
       sendTemperatureAndHumidityToDashboard();
 
